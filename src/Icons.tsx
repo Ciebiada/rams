f69c57cdb105a99b0e298ef0f8302d9b2b03e7d3
Michał Ciebiada <ciebiada@gmail.com>
2025-12-26 16:13:43 +0100

    Add add icon

--- src/Icons.tsx
+++ src/Icons.tsx
@@ -140,3 +140,21 @@ export const CheckIcon = () => (
     />
   </svg>
 );
+
+export const AddIcon = () => (
+  <svg
+    width="24"
+    height="24"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="32"
+      d="M256 112v288M400 256H112"
+    />
+  </svg>
+);
